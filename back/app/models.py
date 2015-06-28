5de2e9c110f471dfecb56880d0dcc89fe185928f
Eric Ahn <wchill1337@gmail.com>
2015-06-28 07:03:20 -0500

    Add ordering, update endpoints

--- back/app/models.py
+++ back/app/models.py
@@ -1,4 +1,5 @@
 from app import db
+import time
 
 DAYS = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
 
@@ -39,14 +40,25 @@ class Orders(db.Model):
     fulfilled = db.Column(db.Boolean, unique=False)
     timestamp = db.Column(db.Integer, unique=False)
 
+    def __init__(self, customer_name, medicine_name, qty, pharmacy_id):
+        self.pharm_id = pharmacy_id
+        self.customer = customer_name
+        self.medicine = medicine_name
+        self.quantity = qty
+        inv = Inventory.query.filter(Inventory.pharm_id == self.pharm_id).filter(Inventory.name == self.medicine).first()
+        self.price = inv.price * qty
+        self.fulfilled = False
+        self.timestamp = int(time.time())
+
     def __repr__(self):
-        return '<Order {} at {} - {} ({} for ${}) for {} at {} ({})'.format(id, pharm_id, medicine, quantity, price, customer, timestamp, 'Fulfilled' if fulfilled else 'Unfulfilled')
+        return '<Order {} at {} - {} ({} for ${}) for {} at {} ({})'.format(self.id, self.pharm_id, self.medicine, self.quantity, self.price, self.customer, self.timestamp, 'Fulfilled' if self.fulfilled else 'Unfulfilled')
 
 class Inventory(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     pharm_id = db.Column(db.Integer, db.ForeignKey('pharmacy.id'), unique=False)
     name = db.Column(db.String(128), unique=False)
     serial = db.Column(db.Integer, unique=False)
+    price = db.Column(db.Float, unique=False)
 
     def __repr__(self):
-        return '<Inventory {} - Pharmacy {} - {} ({})'.format(id, pharm_id, name, serial)
+        return '<Inventory {} - Pharmacy {} - {} ({}, ${})'.format(self.id, self.pharm_id, self.name, self.serial, self.price)
